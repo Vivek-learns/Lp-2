df4ce950628856e2a0bc8cb03f51dc5ea6821202
Vivek Pandit <vickypandit.nsk@gmail.com>
2026-05-15 11:27:15 +0530

    Add basic C++ program for addition of two numbers

---
+++ add.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+  int n1,n2;
+  
+  cout<<"enter the number: ";
+  cin>>n1;
+  cout<<"enter the second number: ";
+  cin>>n2;
+  cout<<"Addition of the two numbers is: "<<n1+n2;
+
+  return 0;
+} 
