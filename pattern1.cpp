6d9d0c843ebd0958df9b87cea49c40c18a4f00f4
Vivek Pandit <vickypandit.nsk@gmail.com>
2026-05-15 12:45:30 +0530

    Add pattern1.cpp for diamond pattern printing
    Implement a pattern printing program that outputs a diamond shape using asterisks.

---
+++ pattern1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  for(int i=1;i<=5;i++) {
+    for(int j=5-i;j>=1;j--){
+      cout<<"  ";
+    }
+    for(int k=1;k<=i*2;k++){
+      cout<<"* ";
+    }
+    cout<<""<<endl;
+  }
+
+    for(int i=4;i>=1;i--) {
+    for(int j=4-i;j>=1;j--){
+      cout<<"  ";
+    }
+    for(int k=1;k<=i*2;k++){
+      cout<<"* ";
+    }
+    cout<<""<<endl;
+  }
+  
+  return 0;
+}
